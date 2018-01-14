7841ad27d61d262191138367eea6a0abe0872071
CHARLONCyril <cyril0308@gmail.com>
2018-01-14 23:44:59 +0100

    acceuil

---
+++ AcceuilAlien.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<link rel="stylesheet" type="text/css" href="AcceuilAlien.css" />
+		<title>ALIEN 6</title>
+	</head>
+
+    <body>
+	<a href="AcceuilAlien.php"><img class="title" src="Alien6image.png"></a>
+		
+		<ul class="menu">
+			<li><a href="Obsolescence.php">Obsolescence d'un logiciel</a></li>
+			<li><a href="Alien6Version.php">Versions d'un logiciel</a></li>
+		</ul>
+		<div class="corps">
+			<h3><u>Veuillez choisir l'option pour votre logiciel :</u></h3>
+			<a href="Obsolescence.php"><img class="choix" src="obsolescence.png"></a>
+			<a href="Alien6Version.php"><img class="choix" src="version.png"></a>
+		</div>
+    </body>
+    
+</html>
